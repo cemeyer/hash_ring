cemeyer/hash_ring

commit ed9a7dcca095fb455b02179de328229010028cca

Fix #5 - Add/test weighted hashing
Add a few tests in test/t_biased.c.

API is: hash_ring_add(ring, value, <weight pct>, ...). You can increase
a value's weight without removing it first, but to decrease, you must
remove and then re-add.

hash_ring_remove remains the same.

Makefile
@@ -7,13 +7,16 @@ test/t_hashring.o: test/t_hashring.c test/t_bias.h include/hashring.h
 test/t_bias.o: test/t_bias.c include/hashring.h
 	make -C test t_bias.o
 
+test/t_biased.o: test/t_biased.c include/hashring.h
+	make -C test t_biased.o
+
 test/isi_hash.o: test/isi_hash.c test/isi_hash.h
 	make -C test isi_hash.o
 
 dep/mh3/MurmurHash3.o: dep/mh3/MurmurHash3.cpp
 	make -C dep mh3/MurmurHash3.o
 
-test/run_tests: test/t_bias.o test/t_hashring.o dep/mh3/MurmurHash3.o src/hashring.o test/isi_hash.o
+test/run_tests: test/t_bias.o test/t_hashring.o dep/mh3/MurmurHash3.o src/hashring.o test/isi_hash.o test/t_biased.o
 	make -C test run_tests
 
 check: test/run_tests

commit 2b899c2b222b49abe5fec0322bf43dacf718810a

Fix #6, as well as an issue introduced in ed9a7dcc
Issue from ed9a7dcc is that rehash() is totally broken for non-100
weights. Unit tests didn't catch that until I added some more for
(new) non-0 remove().

Notable changes:
- Store weights in hash ring itself. This allows us to rehash without
  keeping a seperate list of members and weights. Less juggling of
  memory, more expensive on the CPU. I think it is the right tradeoff
  for code complexity given my use doesn't add/remove often.
  * As a result, members are limited to 24 bits. Shouldn't impact my use
    noticably, however the unit tests assumed 32-bits in many places.
- We can no longer track hr_count (number of distinct members) cheaply.
  Dropped.
- hash_ring_getn() is a lot more expensive (O(n)) if you request an
  unsatisfiable number of members. I suggest you avoid making such a
  request.
- Add/remove gained an O(N+M) fixup pass; this should have minor impact,
  since they were and still are O(N*M) routines.

Update README to not mention 32-bit members.

Makefile
@@ -9,7 +9,7 @@ hashring.o: hashring.c hashring.h
 	$(CC) $(CFLAGS) -c $<
 
 T_DEPS = hashring.o MurmurHash3.o siphash24.o isi_hash.o
-T_OBJS = t_bias.o t_biased.o t_hashring.o $(T_DEPS)
+T_OBJS = t_bias.o t_hashring.o t_weights.o $(T_DEPS)
 T_HDRS = t_bias.h siphash24.h hashring.h isi_hash.h MurmurHash3.h
 
 run_tests: $(T_OBJS) $(T_HDRS)

hashring.c
@@ -51,6 +51,16 @@ struct malloc_type;
 
 #include "hashring.h"
 
+/* Extract weight, value from combined field in hr_kv_pair */
+#define HR_VAL_BITS		24
+#define HR_VAL_MASK		((1U << HR_VAL_BITS) - 1)
+#define HR_VAL(u32val)		((u32val) & HR_VAL_MASK)
+#define HR_WEIGHT(u32val)	((u32val) >> HR_VAL_BITS)
+
+/* Combine weight and 24-bit value into combined kv_value */
+#define HR_MK_VAL(u32wt, u32member) \
+	(((u32wt) << HR_VAL_BITS) | HR_VAL(u32member))
+
 static void	*bsearch_or_next(const void *key, const void *base,
 				 size_t nmemb, size_t size,
 				 int (*cmp)(const void *, const void *));
@@ -62,6 +72,7 @@ static void	 remove_ring_item(struct hash_ring *, uint32_t hash,
 				  uint32_t member);
 
 static void	 rehash(struct hash_ring *, uint32_t *memb);
+static void	 ring_fixup_weights(struct hash_ring*, uint32_t mempair);
 
 /*
  * =========================================
@@ -77,7 +88,6 @@ hash_ring_init(struct hash_ring *h, hr_hasher_t hash, struct malloc_type *mt,
 	h->hr_hash_fn = hash;
 	h->hr_mtype = mt;
 	h->hr_nreplicas = nreplicas;
-	h->hr_count = 0;
 
 	h->hr_ring = NULL;
 	h->hr_ring_used = 0;
@@ -107,12 +117,13 @@ hash_ring_add(struct hash_ring *h, uint32_t member, unsigned weightpct,
 {
 	uint8_t hashdata[8];
 	uint32_t reps;
-	size_t hr_used, need;
+	size_t need;
 
 #ifdef INVARIANTS
 	ASSERT(h->hr_initialized);
 #endif
 	ASSERT(weightpct > 0 && weightpct <= 100);
+	ASSERT(HR_WEIGHT(member) == 0);
 
 	need = (h->hr_ring_used + h->hr_nreplicas) * sizeof(h->hr_ring[0]);
 
@@ -134,8 +145,6 @@ hash_ring_add(struct hash_ring *h, uint32_t member, unsigned weightpct,
 		return need;
 	}
 
-	hr_used = h->hr_ring_used;
-
 	le32enc(hashdata, member);
 
 	reps = weightpct * h->hr_nreplicas / 100;
@@ -151,8 +160,7 @@ hash_ring_add(struct hash_ring *h, uint32_t member, unsigned weightpct,
 		add_ring_item(h, rhash, member);
 	}
 
-	if (hr_used != h->hr_ring_used)
-		h->hr_count++;
+	ring_fixup_weights(h, HR_MK_VAL(weightpct, member));
 
 	return 0;
 }
@@ -163,15 +171,19 @@ hash_ring_add(struct hash_ring *h, uint32_t member, unsigned weightpct,
  * aux is always freed.
  */
 size_t
-hash_ring_remove(struct hash_ring *h, uint32_t member, void *aux, size_t auxsz)
+hash_ring_remove(struct hash_ring *h, uint32_t member, unsigned weightpct,
+    void *aux, size_t auxsz)
 {
 	uint8_t hashdata[8];
 	size_t hr_used,
 	       memb_exp;
+	uint32_t reps, i;
 
 #ifdef INVARIANTS
 	ASSERT(h->hr_initialized);
 #endif
+	ASSERT(weightpct < 100);
+	ASSERT(HR_WEIGHT(member) == 0);
 
 	memb_exp = 0;
 	if (h->hr_nreplicas > 0)
@@ -187,7 +199,12 @@ hash_ring_remove(struct hash_ring *h, uint32_t member, void *aux, size_t auxsz)
 	hr_used = h->hr_ring_used;
 
 	le32enc(hashdata, member);
-	for (uint32_t i = 0; i < h->hr_nreplicas; i++) {
+
+	reps = weightpct * h->hr_nreplicas / 100;
+	if (reps == 0 && weightpct > 0)
+		reps = 1;
+
+	for (i = h->hr_nreplicas - 1; i >= reps && i < UINT32_MAX; i--) {
 		uint32_t rhash;
 
 		le32enc(&hashdata[4], i);
@@ -196,15 +213,12 @@ hash_ring_remove(struct hash_ring *h, uint32_t member, void *aux, size_t auxsz)
 		remove_ring_item(h, rhash, member);
 	}
 
+	ring_fixup_weights(h, HR_MK_VAL(weightpct, member));
+
 	/* TODO: possibly shrink h->hr_ring at this point if underfull */
 
-	if (hr_used != h->hr_ring_used) {
-		h->hr_count--;
-
-		/* A collision and then removal happened? Re-hash everything */
-		if (h->hr_ring_used % h->hr_nreplicas != 0)
-			rehash(h, aux);
-	}
+	if (hr_used != h->hr_ring_used)
+		rehash(h, aux);
 
 	if (aux != NULL)
 		free(aux, h->hr_mtype);
@@ -217,7 +231,7 @@ hash_ring_getn(struct hash_ring *h, uint32_t hash, unsigned n,
     uint32_t *memb_out)
 {
 	int error = EINVAL;
-	uint32_t i, found;
+	uint32_t i, found, walked;
 	struct hr_kv_pair *bucket, pairkey;
 
 #ifdef INVARIANTS
@@ -226,11 +240,6 @@ hash_ring_getn(struct hash_ring *h, uint32_t hash, unsigned n,
 
 	if (n == 0)
 		goto out;
-
-	if (h->hr_count < n) {
-		error = ENOENT;
-		goto out;
-	}
 
 	error = 0;
 
@@ -250,11 +259,28 @@ hash_ring_getn(struct hash_ring *h, uint32_t hash, unsigned n,
 	 * We start with hr_ring[i], walking until we find enough distinct
 	 * items
 	 */
+	walked = 0;
 	for (found = 0; n > found; i = (i + 1) % h->hr_ring_used) {
 		bool already_found = false;
 
+		/*
+		 * Since we no longer have a reliable hash member count, error
+		 * out if we walk the whole ring and don't have enough members
+		 * to satisfy the request.
+		 *
+		 * Since this may be expensive (O(N) instead of amortized
+		 * O(1)), callers are advised to only call getn() with N <= the
+		 * number of members they have inserted. This is often easy for
+		 * the user to track.
+		 */
+		if (walked >= h->hr_ring_used) {
+			error = ENOENT;
+			goto out;
+		}
+		walked++;
+
 		for (unsigned j = 0; j < found; j++) {
-			if (memb_out[j] == h->hr_ring[i].kv_value) {
+			if (memb_out[j] == HR_VAL(h->hr_ring[i].kv_value)) {
 				already_found = true;
 				break;
 			}
@@ -263,7 +289,7 @@ hash_ring_getn(struct hash_ring *h, uint32_t hash, unsigned n,
 		if (already_found)
 			continue;
 
-		memb_out[found] = h->hr_ring[i].kv_value;
+		memb_out[found] = HR_VAL(h->hr_ring[i].kv_value);
 		found++;
 	}
 
@@ -363,9 +389,10 @@ hr_kv_cmp(const void *a, const void *b)
  * Insert a new mapping into the ordered map internal to this hash_ring.
  */
 static void
-add_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member)
+add_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member_)
 {
 	struct hr_kv_pair *insert, *end, newpair;
+	uint32_t member = HR_VAL(member_);
 
 	ASSERT_DEBUG(h->hr_ring_capacity - h->hr_ring_used >= 1);
 	
@@ -380,8 +407,8 @@ add_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member)
 
 	if (insert != end && insert->kv_hash == hash) {
 		/* Collision on 'hash', lowest value wins */
-		if (member < insert->kv_value)
-			insert->kv_value = member;
+		if (member < HR_VAL(insert->kv_value))
+			insert->kv_value = member_;
 		return;
 	}
 
@@ -397,9 +424,10 @@ add_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member)
 }
 
 static void
-remove_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member)
+remove_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member_)
 {
 	struct hr_kv_pair *remove, *end, rpair;
+	uint32_t member = HR_VAL(member_);
 
 	rpair.kv_hash = hash;
 	rpair.kv_value = member;
@@ -408,7 +436,7 @@ remove_ring_item(struct hash_ring *h, uint32_t hash, uint32_t member)
 	remove = bsearch(&rpair, h->hr_ring, h->hr_ring_used,
 	    sizeof(struct hr_kv_pair), hr_kv_cmp);
 
-	if (remove == NULL || remove->kv_value != member)
+	if (remove == NULL || HR_VAL(remove->kv_value) != member)
 		return;
 
 	if (remove + 1 != end)
@@ -424,6 +452,7 @@ rehash(struct hash_ring *h, uint32_t *memb)
 	uint8_t hashdata[8];
 	unsigned nmemb = 0,
 		 i, j;
+	uint32_t reps;
 
 	/* Extract all members... */
 	for (i = 0; i < h->hr_ring_used; i++) {
@@ -433,18 +462,23 @@ rehash(struct hash_ring *h, uint32_t *memb)
 			if (memb[j] == m)
 				break;
 		if (j >= nmemb) {
-			ASSERT_DEBUG(j < h->hr_count);
 			memb[j] = m;
 			nmemb++;
 		}
-		if (nmemb == h->hr_count)
-			break;
 	}
 
 	/* Re-add all hashes... hurray */
 	for (i = 0; i < nmemb; i++) {
-		le32enc(hashdata, memb[i]);
-		for (uint32_t r = 0; r < h->hr_nreplicas; r++) {
+		unsigned weightpct;
+
+		le32enc(hashdata, HR_VAL(memb[i]));
+		weightpct = HR_WEIGHT(memb[i]);
+
+		reps = weightpct * h->hr_nreplicas / 100;
+		if (reps == 0)
+			reps = 1;
+
+		for (uint32_t r = 0; r < reps; r++) {
 			uint32_t rhash;
 
 			le32enc(&hashdata[4], r);
@@ -452,8 +486,16 @@ rehash(struct hash_ring *h, uint32_t *memb)
 			add_ring_item(h, rhash, memb[i]);
 		}
 	}
+}
 
-	/* Something was completely hashed over? */
-	if (nmemb < h->hr_count)
-		h->hr_count = nmemb;
+static void
+ring_fixup_weights(struct hash_ring *h, uint32_t mempair)
+{
+	struct hr_kv_pair *it, *end;
+	uint32_t member = HR_VAL(mempair);
+
+	end = &h->hr_ring[h->hr_ring_used];
+	for (it = h->hr_ring; it < end; it++)
+		if (HR_VAL(it->kv_value) == member)
+			it->kv_value = mempair;
 }

hashring.h
@@ -51,27 +51,30 @@ void	hash_ring_init(struct hash_ring *h, hr_hasher_t hash,
 void	hash_ring_clean(struct hash_ring *h);
 
 /*
- * Adds a member to hash_ring @h, weight by @weightpct (1 to 100). Adds of the
- * same member and weight are idempotent; additionally, increasing the weight
- * of a member without first removing it should work. Decreasing weight
- * requires removing the member first.
+ * Increases the @weightpct (1-100) of @member in @h (potentially from zero,
+ * i.e., not present). If member's weight is greater than @weightpct, does
+ * nothing. A non-zero @weightpct will always round up to at least one entry in
+ * the ring.
  *
- * Given a buf 'newmemb' (can be NULL) and size of buf sz (can be zero), adds
- * member to h. If newmemb isn't big enough, fails and returns a size of buffer
- * for caller to allocate. On success, returns zero.
+ * If newmemb isn't big enough, fails and returns a size of buffer for caller
+ * to allocate. On success, returns zero.
+ *
+ * Only the low 24 bits of member are usable.
  */
 size_t	hash_ring_add(struct hash_ring *h, uint32_t member, unsigned weightpct,
 		      void *newmemb, size_t sz);
 
 /*
- * Remove a member from the hash_ring @h. Removes are idempotent, so removing a
- * non-existent member is a no-op.
+ * Decreases the @weightpct (0-99) of @member in @h (potentially to zero, i.e.,
+ * full removal). If the member is already absent or its weight is less than
+ * @weightpct, does nothing. As with hash_ring_add(), a non-zero @weightpct
+ * will always round up to at least one entry in the ring.
  *
  * Like add(), takes an auxiliary buf and returns a new size if this one isn't
  * big enough. The passed buf is always freed. On success, returns zero.
  */
-size_t	hash_ring_remove(struct hash_ring *h, uint32_t member, void *aux,
-			 size_t sz);
+size_t	hash_ring_remove(struct hash_ring *h, uint32_t member,
+			 unsigned weightpct, void *aux, size_t sz);
 
 /*
  * Gets @n (1 or more) replicas from the hash_ring @h appropriate for key
@@ -119,9 +122,6 @@ struct hash_ring {
 	/* In units of struct hr_kv_pair: */
 	size_t			 hr_ring_used;
 	size_t			 hr_ring_capacity;
-
-	/* No. of members of this ring */
-	uint32_t		 hr_count;
 
 	/* No. of replicas per member in map */
 	uint32_t		 hr_nreplicas;

t_bias.h
@@ -13,7 +13,7 @@
 
 /* Add bias tests to check suite */
 void suite_add_t_bias(Suite *s);
-void suite_add_t_biased(Suite *s);
+void suite_add_t_weights(Suite *s);
 
 extern const struct hash_compare {
 	const char	*name;

t_hashring.c
@@ -28,15 +28,18 @@
 fail_if(hash_ring_add(r, m, 100/*weight*/, malloc(NBYTES), NBYTES))
 
 #define hash_ring_remove(r, m) \
-fail_if(hash_ring_remove(r, m, malloc(NBYTES), NBYTES))
+fail_if(hash_ring_remove(r, m, 0/*weight*/, malloc(NBYTES), NBYTES))
 
+#define silence_hash_ring_dump 1
+#define skip_noisy_tests 1
+
+#if !silence_hash_ring_dump
 static void
 _hash_ring_dump(struct hash_ring *h)
 {
 
 	printf("%p -> {\n", h);
 	printf("\thash_fn: %p\n", h->hr_hash_fn);
-	printf("\tcount: %"PRIu32"\n", h->hr_count);
 	printf("\treplicas: %"PRIu32"\n", h->hr_nreplicas);
 	printf("\tring:\n");
 	for (size_t i = 0; i < h->hr_ring_used; i++)
@@ -44,6 +47,7 @@ _hash_ring_dump(struct hash_ring *h)
 		    h->hr_ring[i].kv_hash, h->hr_ring[i].kv_value);
 	printf("\tring capacity: %zu\n", h->hr_ring_capacity);
 }
+#endif
 
 #define hasher md5_hasher
 
@@ -63,10 +67,10 @@ START_TEST(basic_additions)
 
 	hash_ring_init(&ring, hasher, 5);
 
-	hash_ring_add(&ring, 0xdeadbeef);
+	hash_ring_add(&ring, 0xdeadbf);
 	hash_ring_add(&ring, 0xc0ffee);
 
-	fail_unless(ring.hr_count == 2);
+	fail_unless(ring.hr_ring_used == 10);
 	hash_ring_clean(&ring);
 }
 END_TEST
@@ -77,20 +81,20 @@ START_TEST(basic_removes)
 
 	hash_ring_init(&ring, hasher, 128);
 
-	hash_ring_add(&ring, 0x124dbeef);
-	hash_ring_add(&ring, 0x0ff426ee);
-	hash_ring_add(&ring, 0x2349620e);
+	hash_ring_add(&ring, 0x124dbe);
+	hash_ring_add(&ring, 0x0ff426);
+	hash_ring_add(&ring, 0x234962);
 
-	fail_unless(ring.hr_count == 3);
+	fail_unless(ring.hr_ring_used == 3*128);
 
-	hash_ring_remove(&ring, 0x2349620e);
-	fail_unless(ring.hr_count == 2);
+	hash_ring_remove(&ring, 0x234962);
+	fail_unless(ring.hr_ring_used == 2*128);
 
-	hash_ring_remove(&ring, 0x124dbeef);
-	fail_unless(ring.hr_count == 1);
+	hash_ring_remove(&ring, 0x124dbe);
+	fail_unless(ring.hr_ring_used == 128);
 
-	hash_ring_remove(&ring, 0x0ff426ee);
-	fail_unless(ring.hr_count == 0);
+	hash_ring_remove(&ring, 0x0ff426);
+	fail_unless(ring.hr_ring_used == 0);
 
 	hash_ring_clean(&ring);
 }
@@ -106,6 +110,7 @@ static const uint32_t characters[] = {
 	0x77700000,
 };
 
+#if !skip_noisy_tests
 START_TEST(example_plain)
 {
 	struct hash_ring ring;
@@ -116,7 +121,7 @@ START_TEST(example_plain)
 	hash_ring_add(&ring, 0xBBBB);
 	hash_ring_add(&ring, 0xCCCC);
 
-#if 0
+#if !silence_hash_ring_dump
 	_hash_ring_dump(&ring);
 #endif
 
@@ -255,6 +260,7 @@ START_TEST(example_remove)
 	hash_ring_clean(&ring);
 }
 END_TEST
+#endif /* !skip_noisy_tests */
 
 static void
 ring_is_sorted(struct hash_ring *ring)
@@ -274,14 +280,14 @@ START_TEST(func_add)
 
 	hash_ring_init(&ring, hasher, 128);
 
-	hash_ring_add(&ring, 0xABCDEF0);
+	hash_ring_add(&ring, 0xABCDEF);
 
 	fail_unless(ring.hr_ring_used == ring.hr_nreplicas);
 	fail_if(ring.hr_ring == NULL);
 
 	ring_is_sorted(&ring);
 
-	hash_ring_add(&ring, 0x1C0FED0);
+	hash_ring_add(&ring, 0x1C0FED);
 
 	/* This can fail in unlikely event of a collision: */
 	fail_unless(ring.hr_ring_used == 2*ring.hr_nreplicas);
@@ -298,8 +304,8 @@ START_TEST(func_remove)
 
 	hash_ring_init(&ring, hasher, 128);
 
-	hash_ring_add(&ring, 0xABCDEF0);
-	hash_ring_remove(&ring, 0xABCDEF0);
+	hash_ring_add(&ring, 0xABCDEF);
+	hash_ring_remove(&ring, 0xABCDEF);
 
 	fail_unless(ring.hr_ring_used == 0);
 	hash_ring_clean(&ring);
@@ -312,8 +318,8 @@ START_TEST(func_remove_nonexist)
 
 	hash_ring_init(&ring, hasher, 128);
 
-	hash_ring_add(&ring, 0xABCDEF0);
-	hash_ring_remove(&ring, 0xFEDCBAF0);
+	hash_ring_add(&ring, 0xABCDEF);
+	hash_ring_remove(&ring, 0xFEDCBA);
 
 	fail_unless(ring.hr_ring_used == ring.hr_nreplicas);
 	fail_if(ring.hr_ring == NULL);
@@ -342,8 +348,13 @@ fill_lotsa_inputs(void)
 	FILE *f;
 
 	f = fopen("/dev/urandom", "rb");
-	for (unsigned i = 0; i < NELEM(_lotsa_inputs); i++)
-		fread(&_lotsa_inputs[i], 4, 1, f);
+	for (unsigned i = 0; i < NELEM(_lotsa_inputs); i++) {
+		size_t rd;
+
+		rd = fread(&_lotsa_inputs[i], 4, 1, f);
+		if (rd != 1)
+			abort();
+	}
 
 	fclose(f);
 }
@@ -356,7 +367,7 @@ for (uint32_t *_INP = _lotsa_inputs; _INP != &_lotsa_inputs[NELEM(_lotsa_inputs)
 START_TEST(func_getsingle)
 {
 	struct hash_ring ring;
-	const uint32_t the_bin = 0xABCDEF0;
+	const uint32_t the_bin = 0xABCDEF;
 
 	hash_ring_init(&ring, hasher, 128);
 
@@ -380,9 +391,9 @@ END_TEST
 START_TEST(func_get_multiple)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 	uint32_t bin;
 	int err;
 
@@ -396,19 +407,19 @@ START_TEST(func_get_multiple)
 	hash_ring_add(&ring, bin2);
 	hash_ring_add(&ring, bin3);
 
-#if 0
+#if !silence_hash_ring_dump
 	_hash_ring_dump(&ring);
 #endif
 
 	err = hash_ring_getn(&ring, 0xbcfdda1d, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin1, "#1 got bin: %08x", bin);
+	fail_if(bin != bin2, "#1 got bin: %08x", bin);
 
 	err = hash_ring_getn(&ring, 0x5, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin2, "#2 got bin: %08x", bin);
+	fail_if(bin != bin1, "#2 got bin: %08x", bin);
 
-	err = hash_ring_getn(&ring, 0x12f9578, 1, &bin);
+	err = hash_ring_getn(&ring, 0xefffffff, 1, &bin);
 	fail_if(err);
 	fail_if(bin != bin3, "#3 got bin: %08x", bin);
 	hash_ring_clean(&ring);
@@ -418,9 +429,9 @@ END_TEST
 START_TEST(func_get_multiple_quick)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 	uint32_t bin;
 	int err;
 
@@ -442,9 +453,9 @@ END_TEST
 START_TEST(func_get_multiple_remove)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 	uint32_t bin;
 	int err;
 
@@ -460,31 +471,31 @@ START_TEST(func_get_multiple_remove)
 
 	err = hash_ring_getn(&ring, 0xbcfdda1d, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin1, "#1 got bin: %08x", bin);
+	fail_if(bin != bin2, "#1 got bin: %08x", bin);
 
 	err = hash_ring_getn(&ring, 0x5, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin2, "#2 got bin: %08x", bin);
+	fail_if(bin != bin1, "#2 got bin: %08x", bin);
 
 	err = hash_ring_getn(&ring, 0x12f9578, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin3, "#3 got bin: %08x", bin);
+	fail_if(bin != bin1, "#3 got bin: %08x", bin);
 
 	/* remove one */
-	hash_ring_remove(&ring, bin2);
+	hash_ring_remove(&ring, bin1);
 
 	err = hash_ring_getn(&ring, 0xbcfdda1d, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin1, "#4 got bin: %08x", bin);
+	fail_if(bin != bin2, "#4 got bin: %08x", bin);
 
 	/* no longer hashes to removed bin */
 	err = hash_ring_getn(&ring, 0x5, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin1, "#5 got bin: %08x", bin);
+	fail_if(bin != bin2, "#5 got bin: %08x", bin);
 
 	err = hash_ring_getn(&ring, 0x12f9578, 1, &bin);
 	fail_if(err);
-	fail_if(bin != bin3, "#6 got bin: %08x", bin);
+	fail_if(bin != bin2, "#6 got bin: %08x", bin);
 	hash_ring_clean(&ring);
 }
 END_TEST
@@ -492,9 +503,9 @@ END_TEST
 START_TEST(func_get_multiple_remove_quick)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 
 	hash_ring_init(&ring, hasher, 128);
 
@@ -520,9 +531,9 @@ END_TEST
 START_TEST(func_get_two)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 
 	hash_ring_init(&ring, md5_hasher, 128);
 
@@ -537,8 +548,8 @@ START_TEST(func_get_two)
 	fail_if(err);
 	fail_if(bins[0] == bins[1]);
 	/* only going to work for a particular hash function... */
-	fail_if(bins[0] != bin1, "1 0x%08x", bins[0]);
-	fail_if(bins[1] != bin2, "2 0x%08x", bins[1]);
+	fail_if(bins[0] != bin2, "1 0x%08x", bins[0]);
+	fail_if(bins[1] != bin1, "2 0x%08x", bins[1]);
 	hash_ring_clean(&ring);
 }
 END_TEST
@@ -546,9 +557,9 @@ END_TEST
 START_TEST(func_get_two_quick)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE,
-	      bin3 = 0x8000F000;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE,
+	      bin3 = 0x80F000;
 	uint32_t bins[2];
 	int err;
 
@@ -572,8 +583,8 @@ END_TEST
 START_TEST(func_get_two_quick2)
 {
 	struct hash_ring ring;
-	const uint32_t bin1 = 0xABCDEF00,
-	      bin2 = 0xDEFC0FEE;
+	const uint32_t bin1 = 0xABCDEF,
+	      bin2 = 0xDC0FEE;
 	uint32_t bins[2];
 	int err;
 
@@ -600,7 +611,7 @@ START_TEST(err_get_two_with_one_in_ring)
 	uint32_t bin;
 
 	hash_ring_init(&ring, hasher, 128);
-	hash_ring_add(&ring, 0xABCDEF00);
+	hash_ring_add(&ring, 0xABCDEF);
 
 	err = hash_ring_getn(&ring, 0x0, 2, &bin);
 	fail_unless(err == ENOENT);
@@ -697,8 +708,14 @@ START_TEST(distribution)
 	double *dist;
 
 	f = fopen("/dev/urandom", "rb");
-	for (unsigned i = 0; i < NELEM(bins); i++)
-		fread(&bins[i], 4, 1, f);
+	for (unsigned i = 0; i < NELEM(bins); i++) {
+		size_t rd;
+
+		rd = fread(&bins[i], 4, 1, f);
+		fail_unless(rd == 1);
+
+		bins[i] &= (1<<24) - 1;
+	}
 	fclose(f);
 
 	dist = malloc(sizeof(double) * NELEM(bins) * NELEM(comparison_functions) * NELEM(comparison_replicas));
@@ -776,20 +793,20 @@ START_TEST(err_idempotent)
 
 	hash_ring_init(&ring, isi_hasher64, 64);
 
-	fail_unless(ring.hr_count == 0);
-	hash_ring_add(&ring, 0x12345678);
-	fail_unless(ring.hr_count == 1);
+	fail_unless(ring.hr_ring_used == 0);
+	hash_ring_add(&ring, 0x123456);
+	fail_unless(ring.hr_ring_used == 64);
 
 	/* adding the same item? don't count it */
-	hash_ring_add(&ring, 0x12345678);
-	fail_unless(ring.hr_count == 1);
+	hash_ring_add(&ring, 0x123456);
+	fail_unless(ring.hr_ring_used == 64);
 
-	hash_ring_remove(&ring, 0x12345678);
-	fail_unless(ring.hr_count == 0);
+	hash_ring_remove(&ring, 0x123456);
+	fail_unless(ring.hr_ring_used == 0);
 
 	/* removing the same item also works. */
-	hash_ring_remove(&ring, 0x12345678);
-	fail_unless(ring.hr_count == 0);
+	hash_ring_remove(&ring, 0x123456);
+	fail_unless(ring.hr_ring_used == 0);
 
 	hash_ring_clean(&ring);
 }
@@ -863,7 +880,7 @@ main(void)
 	tcase_add_test(t, basic_removes);
 	suite_add_tcase(s, t);
 
-#if 0
+#if !skip_noisy_tests
 	t = tcase_create("useless_printfs");
 	tcase_add_test(t, example_plain);
 	tcase_add_test(t, example_adds);
@@ -898,7 +915,7 @@ main(void)
 	suite_add_tcase(s, t);
 
 	suite_add_t_bias(s);
-	suite_add_t_biased(s);
+	suite_add_t_weights(s);
 
 	SRunner *sr = srunner_create(s);
 	srunner_run_all(sr, CK_VERBOSE);

t_weights.c
@@ -29,8 +29,8 @@
 #define hash_ring_add(r, m, w) \
 fail_if(hash_ring_add(r, m, w, malloc(NBYTES), NBYTES))
 
-#define hash_ring_remove(r, m) \
-fail_if(hash_ring_remove(r, m, malloc(NBYTES), NBYTES))
+#define hash_ring_remove(r, m, w) \
+fail_if(hash_ring_remove(r, m, w, malloc(NBYTES), NBYTES))
 
 #define min(x, y) ({ \
 	typeof(x) _min1 = (x); \
@@ -44,13 +44,15 @@ fail_if(hash_ring_remove(r, m, malloc(NBYTES), NBYTES))
 	(void) (&_max1 == &_max2); \
 	_max1 > _max2 ? _max1 : _max2; })
 
+#define silence_hash_ring_dump 1
+
+#if !silence_hash_ring_dump
 static void
 _hash_ring_dump(struct hash_ring *h)
 {
 
 	printf("%p -> {\n", h);
 	printf("\thash_fn: %p\n", h->hr_hash_fn);
-	printf("\tcount: %"PRIu32"\n", h->hr_count);
 	printf("\treplicas: %"PRIu32"\n", h->hr_nreplicas);
 	printf("\tring:\n");
 	for (size_t i = 0; i < h->hr_ring_used; i++)
@@ -58,15 +60,18 @@ _hash_ring_dump(struct hash_ring *h)
 		    h->hr_ring[i].kv_hash, h->hr_ring[i].kv_value);
 	printf("\tring capacity: %zu\n", h->hr_ring_capacity);
 }
+#endif
 
 static unsigned
 hash_ring_weight(struct hash_ring *h, uint32_t nreplicas, uint32_t member)
 {
+	const uint32_t MASK = (1<<24)-1;
 	unsigned tot = 0;
 
-	for (size_t i = 0; i < h->hr_ring_used; i++)
-		if (h->hr_ring[i].kv_value == member)
+	for (size_t i = 0; i < h->hr_ring_used; i++) {
+		if ((h->hr_ring[i].kv_value & MASK) == member)
 			tot++;
+	}
 
 	return 100 * tot / nreplicas;
 }
@@ -96,16 +101,56 @@ START_TEST(wht_basic)
 
 	hash_ring_init(&ring, isi_hasher64, nreps);
 
-	hash_ring_add(&ring, 0xdeadbeef, 100);
+	hash_ring_add(&ring, 0xdeadbf, 100);
 	hash_ring_add(&ring, 0xc0ffee, 50);
 
-	fail_unless(ring.hr_count == 2);
+#if !silence_hash_ring_dump
+	_hash_ring_dump(&ring);
+#endif
 
-	wt = hash_ring_weight(&ring, nreps, 0xdeadbeef);
+	fail_unless(ring.hr_ring_used == 3*nreps/2);
+
+	wt = hash_ring_weight(&ring, nreps, 0xdeadbf);
 	fail_unless(eps_equals(wt, 100, 1), "wt: %u", wt);
 
 	wt = hash_ring_weight(&ring, nreps, 0xc0ffee);
 	fail_unless(eps_equals(wt, 50, 1), "wt: %u", wt);
+
+	hash_ring_clean(&ring);
+}
+END_TEST
+
+START_TEST(wht_basic_remove)
+{
+	struct hash_ring ring;
+	uint32_t nreps = 64;
+	unsigned wt;
+
+	hash_ring_init(&ring, isi_hasher64, nreps);
+
+	hash_ring_add(&ring, 0xdeadbf, 100);
+	hash_ring_add(&ring, 0xc0ffee, 50);
+
+	fail_unless(ring.hr_ring_used == 3*nreps/2);
+
+	wt = hash_ring_weight(&ring, nreps, 0xdeadbf);
+	fail_unless(eps_equals(wt, 100, 1), "wt: %u", wt);
+
+	wt = hash_ring_weight(&ring, nreps, 0xc0ffee);
+	fail_unless(eps_equals(wt, 50, 1), "wt: %u", wt);
+
+	hash_ring_remove(&ring, 0xdeadbf, 55);
+	hash_ring_remove(&ring, 0xc0ffee, 23);
+
+	wt = hash_ring_weight(&ring, nreps, 0xdeadbf);
+	fail_unless(eps_equals(wt, 55, 3), "wt: %u", wt);
+	wt = hash_ring_weight(&ring, nreps, 0xc0ffee);
+	fail_unless(eps_equals(wt, 23, 3), "wt: %u", wt);
+
+	hash_ring_add(&ring, 0xc0ffee, 100);
+
+	wt = hash_ring_weight(&ring, nreps, 0xc0ffee);
+	fail_unless(eps_equals(wt, 100, 1), "wt: %u", wt);
 
 	hash_ring_clean(&ring);
 }
@@ -119,27 +164,28 @@ START_TEST(wht_bigger)
 
 	hash_ring_init(&ring, isi_hasher64, nreps);
 
-	hash_ring_add(&ring, 0xdeadbeef, 100);
+	hash_ring_add(&ring, 0xdeadbf, 100);
 	hash_ring_add(&ring, 0xc0ffee, 50);
-	hash_ring_add(&ring, 0xdeadb4dd, 21);
-	hash_ring_add(&ring, 0x11220033, 5);
-	hash_ring_add(&ring, 0x71220433, 75);
+	hash_ring_add(&ring, 0xd5adb4, 21);
+	hash_ring_add(&ring, 0x112200, 5);
+	hash_ring_add(&ring, 0x712204, 75);
 
-	fail_unless(ring.hr_count == 5);
+	fail_unless(eps_equals(ring.hr_ring_used, (100+50+21+5+75)*nreps/100,
+		2));
 
-	wt = hash_ring_weight(&ring, nreps, 0xdeadbeef);
+	wt = hash_ring_weight(&ring, nreps, 0xdeadbf);
 	fail_unless(eps_equals(wt, 100, 3), "wt: %u", wt);
 
 	wt = hash_ring_weight(&ring, nreps, 0xc0ffee);
 	fail_unless(eps_equals(wt, 50, 3), "wt: %u", wt);
 
-	wt = hash_ring_weight(&ring, nreps, 0xdeadb4dd);
+	wt = hash_ring_weight(&ring, nreps, 0xd5adb4);
 	fail_unless(eps_equals(wt, 21, 3), "wt: %u", wt);
 
-	wt = hash_ring_weight(&ring, nreps, 0x11220033);
+	wt = hash_ring_weight(&ring, nreps, 0x112200);
 	fail_unless(eps_equals(wt, 5, 3), "wt: %u", wt);
 
-	wt = hash_ring_weight(&ring, nreps, 0x71220433);
+	wt = hash_ring_weight(&ring, nreps, 0x712204);
 	fail_unless(eps_equals(wt, 75, 3), "wt: %u", wt);
 
 	hash_ring_clean(&ring);
@@ -153,7 +199,7 @@ START_TEST(wht_bounds1)
 	stderr = fopen("/dev/null", "w+b");
 	fail_unless((intptr_t)stderr);
 
-	hash_ring_add(NULL, 0xdeadbeef, 101);
+	hash_ring_add(NULL, 0xdeadbf, 101);
 }
 END_TEST
 
@@ -164,19 +210,52 @@ START_TEST(wht_bounds2)
 	stderr = fopen("/dev/null", "w+b");
 	fail_unless((intptr_t)stderr);
 
-	hash_ring_add(NULL, 0xdeadbeef, 0);
+	hash_ring_add(NULL, 0xdeadbf, 0);
+}
+END_TEST
+
+START_TEST(wht_bounds3)
+{
+
+	// Silence assert
+	stderr = fopen("/dev/null", "w+b");
+	fail_unless((intptr_t)stderr);
+
+	hash_ring_remove(NULL, 0xdeadbf, 100);
+}
+END_TEST
+
+START_TEST(wht_getn_terminates)
+{
+	struct hash_ring ring;
+	uint32_t out[10];
+	int rc;
+
+	hash_ring_init(&ring, isi_hasher64, 16);
+
+	hash_ring_add(&ring, 0xdeadbf, 100);
+	hash_ring_add(&ring, 0xc0ffee, 50);
+
+	rc = hash_ring_getn(&ring, 0x11000f, 10, out);
+	fail_unless(rc == ENOENT);
+
+	hash_ring_clean(&ring);
+
 }
 END_TEST
 
 void
-suite_add_t_biased(Suite *s)
+suite_add_t_weights(Suite *s)
 {
 	struct TCase *t;
 
 	t = tcase_create("weighted_hashing");
 	tcase_add_test(t, wht_basic);
+	tcase_add_test(t, wht_basic_remove);
 	tcase_add_test(t, wht_bigger);
 	tcase_add_test_raise_signal(t, wht_bounds1, SIGABRT);
 	tcase_add_test_raise_signal(t, wht_bounds2, SIGABRT);
+	tcase_add_test_raise_signal(t, wht_bounds3, SIGABRT);
+	tcase_add_test(t, wht_getn_terminates);
 	suite_add_tcase(s, t);
 }